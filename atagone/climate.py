da9656c6e58ea3af34fab14152fc8a04d970b766
Wim van den Herik <herikw@users.noreply.github.com>
2019-07-19 20:34:09 +0200

    Updated for latest HA version supporting HVAC mode

--- atagone/climate.py
+++ atagone/climate.py
@@ -1,7 +1,7 @@
 """
 Adds Support for Atag One Thermostat
 
-Author: herikw 
+Author: herikw
 https://github.com/herikw/home-assistant-custom-components
 
 Configuration for this platform:
@@ -19,19 +19,13 @@ import json
 import voluptuous as vol
 import urllib.request
 from urllib.error import HTTPError
-
-from homeassistant.components.climate import (ClimateDevice, PLATFORM_SCHEMA)
-from homeassistant.components.climate.const import ( STATE_COOL, STATE_ECO,
-                                                     STATE_HEAT, STATE_AUTO,
-                                                     SUPPORT_OPERATION_MODE,
-                                                     SUPPORT_TARGET_TEMPERATURE, STATE_MANUAL)
-
-from homeassistant.const import (CONF_NAME, CONF_HOST, CONF_PORT, TEMP_CELSIUS, ATTR_TEMPERATURE)
-import homeassistant.helpers.config_validation as config_validation
-
+from typing import Optional, List
 import requests
 
-SUPPORT_FLAGS = SUPPORT_TARGET_TEMPERATURE | SUPPORT_OPERATION_MODE
+from homeassistant.components.climate import ClimateDevice, PLATFORM_SCHEMA
+from homeassistant.components.climate.const import (SUPPORT_TARGET_TEMPERATURE, HVAC_MODE_HEAT, CURRENT_HVAC_HEAT, CURRENT_HVAC_IDLE)
+from homeassistant.const import CONF_HOST, CONF_PORT, CONF_NAME, TEMP_CELSIUS, ATTR_TEMPERATURE
+import homeassistant.helpers.config_validation as config_validation
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -39,6 +33,16 @@ DEFAULT_NAME = 'Atag One Thermostat'
 DEFAULT_TIMEOUT = 30
 BASE_URL = 'http://{0}:{1}{2}'
 MAC_ADDRESS = '01:23:45:67:89:01'
+DEFAULT_MIN_TEMP = 4
+DEFAULT_MAX_TEMP = 27
+
+SUPPORT_FLAGS = (SUPPORT_TARGET_TEMPERATURE)
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Optional(CONF_NAME, default=DEFAULT_NAME): config_validation.string,
+    vol.Required(CONF_HOST): config_validation.string,
+    vol.Optional(CONF_PORT, default=10000): config_validation.positive_int,
+})
 
 # jsonPayload data templates
 # update payload need to be in exact order. So, using string instead of json.dumps
@@ -93,33 +97,21 @@ PAIR_MESSAGE = '''{{
 }}'''
 
 READ_PATH = '/retrieve'
-UPDATE_PATH = '/update' 
+UPDATE_PATH = '/update'
 PAIR_PATH = '/pair_message'
 MESSAGE_INFO_CONTROL = 1
 MESSAGE_INFO_REPORT = 8
 MESSAGE_INFO_EXTRA = 64
 
-ATTR_MODE = 'mode'
-STATE_MANUAL = 'manual'
-STATE_UNKNOWN = 'unknown'
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Optional(CONF_NAME, default=DEFAULT_NAME): config_validation.string,
-    vol.Required(CONF_HOST): config_validation.string,
-    vol.Optional(CONF_PORT, default=10000): config_validation.positive_int,
-})
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    """Setup the Atag One Device"""
+
+    add_entities([AtagOneThermostat(config.get(CONF_NAME), config.get(CONF_HOST), config.get(CONF_PORT))])
 
 
-# pylint: disable=unused-argument
-def setup_platform(hass, config, add_devices, discovery_info=None):
-    """Setup for the Atag One thermostat."""
-    add_devices([AtagOneThermostat(config.get(CONF_NAME), config.get(CONF_HOST), config.get(CONF_PORT))])
-
-
-# pylint: disable=abstract-method
-# pylint: disable=too-many-instance-attributes
 class AtagOneThermostat(ClimateDevice):
-    """Representation of the ATAG One thermostat."""
+    """Representation of a Atag One device"""
 
     def __init__(self, name, host, port):
         """Initialize"""
@@ -127,15 +119,22 @@ class AtagOneThermostat(ClimateDevice):
         self._name = name
         self._host = host
         self._port = port
+
+        self._min_temp = DEFAULT_MIN_TEMP
+        self._max_temp = DEFAULT_MAX_TEMP
         self._current_temp = None
         self._current_setpoint = None
+        self._paired = False
+        self._heating = False
         self._current_state = -1
         self._current_operation = ''
-        self._set_state = None
-        self._operation_list = ['Manual', 'Auto', 'Holiday', 'Fireplace']
-        self._paired = False
 
         self.update()
+ 
+    @property
+    def supported_features(self):
+        """Return the list of supported features."""
+        return SUPPORT_FLAGS
 
     @staticmethod
     def send_request(self, requestPath, jsonPayload):
@@ -174,28 +173,69 @@ class AtagOneThermostat(ClimateDevice):
 
             self._paired = False
 
+    def update(self):
+        """Update unit attributes."""
+        
+        jsonPayload = json.dumps({
+            'retrieve_message': {
+                'seqnr': 0,
+                'account_auth': {
+                   'user_account': "",
+                    'mac_address': "01:23:45:67:89:01"
+                },
+                'info': MESSAGE_INFO_CONTROL + MESSAGE_INFO_REPORT + MESSAGE_INFO_EXTRA
+            }
+
+        })
+
+        resp = self.send_request(self, READ_PATH, jsonPayload)
+        self._data = resp['retrieve_reply']
+        status = self._data['acc_status']
+
+        if status == 2:
+            self._current_setpoint = self._data['report']['shown_set_temp']
+            self._current_temp = self._data['report']['room_temp']
+            self._current_state = self._data['control']['ch_mode']
+            boiler_status = int(self._data['report']['boiler_status']) & 14
+            _LOGGER.debug("boiler status: %s", boiler_status)
+            if boiler_status == 2:
+               self._heating = True
+            else:
+               self._heating = False
+        else:
+            self.pair_atag()
+            _LOGGER.error("Please accept pairing request on Atag ONE Device")
+
+    @property
+    def hvac_mode(self) -> str:
+        """Return hvac operation """
+        return HVAC_MODE_HEAT
+
+    @property
+    def hvac_modes(self) -> List[str]:
+        """Return the list of available hvac operation modes. Need to be a subset of HVAC_MODES. """
+        return [HVAC_MODE_HEAT]
+
+    def set_hvac_mode(self, hvac_mode: str) -> None:
+        """Set new target hvac mode."""
+        pass
+
+    @property
+    def hvac_action(self) -> Optional[str]:
+        """Return the current running hvac operation if supported.  Need to be one of CURRENT_HVAC_*.  """
+        if self._heating:
+            return CURRENT_HVAC_HEAT
+        return CURRENT_HVAC_IDLE
 
     @property
     def should_poll(self):
-        """Polling needed for thermostat."""
+        """Return the polling state."""
         return True
 
     @property
-    def supported_features(self):
-        """Return the list of supported features."""
-        return SUPPORT_FLAGS
-
-    @property
     def name(self):
-        """Return the name of the thermostat."""
+        """Return the name of the climate device."""
         return self._name
-
-    @property
-    def device_state_attributes(self):
-        """Return the device specific state attributes."""
-        return {
-            ATTR_MODE: self._current_state
-        }
 
     @property
     def temperature_unit(self):
@@ -208,75 +248,25 @@ class AtagOneThermostat(ClimateDevice):
         return self._current_temp
 
     @property
+    def min_temp(self):
+        """Return the minimum temperature."""
+        if self._min_temp:
+            return self._min_temp
+
+    @property
+    def max_temp(self):
+        """Return the maximum temperature."""
+        if self._max_temp:
+            return self._max_temp
+
+    @property
     def target_temperature(self):
         """Return the temperature we try to reach."""
         return self._current_setpoint
 
-    @property
-    def current_operation(self):
-        """Return the current state of the thermostat."""
-        state = self._current_state
-        if state in (1, 2, 3, 5):
-            return self._operation_list[state-1]
-        elif state == -1:
-            return STATE_AUTO
-        else:
-            return STATE_UNKNOWN
-
-    @property
-    def operation_list(self):
-        """List of available operation modes."""
-        return self._operation_list
-
-    def set_operation_mode(self, atag_mode):
-        """Set ATAG ONE mode (auto, manual)."""
-
-        if atag_mode == "Manual":
-            mode = 1
-        elif atag_mode == "Auto":
-            mode = 2
-        elif atag_mode == "Holiday":
-            mode = 3
-        elif atag_mode == "Fireplace":
-            mode = 5
-
-        jsonPayload = UPDATE_MODE.format(MAC_ADDRESS, mode)
-        resp = self.send_request(self, UPDATE_PATH, jsonPayload)
-        self._data = resp['update_reply']
-        status = self._data['acc_status']
-
-        if status != 2:
-            _LOGGER.error("Request Status: %s", status)
-
-    def update(self):
-        """Read data from thermostat."""
-
-        jsonPayload = json.dumps({
-            'retrieve_message': {
-                'seqnr': 0,
-                'account_auth': {
-                   'user_account': "",
-                    'mac_address': "01:23:45:67:89:01"
-                },
-                'info': MESSAGE_INFO_CONTROL + MESSAGE_INFO_REPORT + MESSAGE_INFO_EXTRA
-            }
-        })
-
-        resp = self.send_request(self, READ_PATH, jsonPayload)
-        self._data = resp['retrieve_reply']
-        status = self._data['acc_status']
-
-        if status == 2:
-            self._current_setpoint = self._data['report']['shown_set_temp']
-            self._current_temp = self._data['report']['room_temp']
-            self._current_state = self._data['control']['ch_mode']
-        else:
-            self.pair_atag()
-            _LOGGER.error("Please accept pairing request on Atag ONE Device")
-
     def set_temperature(self, **kwargs):
         """Set new target temperature."""
-
+        
         target_temp = kwargs.get(ATTR_TEMPERATURE)
         if target_temp is None:
             return
@@ -288,5 +278,3 @@ class AtagOneThermostat(ClimateDevice):
 
             if status != 2:
                 _LOGGER.error("Request Status: %s", status)
-
-
